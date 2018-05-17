1de9a959bd9bb307617f0b295dde4f324f0e1a55
Anmol-Singh <anmoldhillon999@gmail.com>
2018-05-17 12:40:47 -0500

    ADDED ANOTHER CAT

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-const catpic = document.querySelector('.catpic')
-const clicks = document.querySelector('.clicks')
+const catpic = document.querySelector('.catpic');
+const clicks = document.querySelector('.clicks');
+const catpic2 = document.querySelector('.catpic2');
+const clicks2 = document.querySelector('.clicks2');
+const catname = document.querySelector('.catname');
+const catname2 = document.querySelector('.catname2');
+const container = document.querySelector('.container');
+
+
 
 catpic.addEventListener('click', function() {
     clicks.innerHTML ++
 })
+
+catpic2.addEventListener('click', function() {
+    clicks2.innerHTML ++
+})
+
+catname.innerHTML = "CAT";
+catname2.innerHTML = "CAT2";
